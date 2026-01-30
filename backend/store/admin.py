bf8b5438c2f54402fc3f25e60141b1c4613e450e
Josue <joflex2000@gmail.com>
2026-01-30 10:10:41 -0300

    Fix: Enable Client Import and preserve UserAdmin functionality

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Product, Order, OrderItem, Category
+from import_export.admin import ImportExportModelAdmin, ImportExportMixin
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(ImportExportMixin, UserAdmin):
     model = CustomUser
     list_display = ['email', 'username', 'role', 'company_name', 'discount_rate', 'is_staff']
     fieldsets = UserAdmin.fieldsets + (
